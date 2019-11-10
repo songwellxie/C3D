71cc8579a46f6777a02260c9bc0454c8f30caeaa
songwellxie <shangyuxie@qq.com>
2019-11-09 22:21:28 -0600

    Update c3d_sport1m_feature_extraction_video.sh

--- C3D-v1.0/examples/c3d_feature_extraction/c3d_sport1m_feature_extraction_video.sh
+++ C3D-v1.0/examples/c3d_feature_extraction/c3d_sport1m_feature_extraction_video.sh
@@ -1,3 +1,7 @@
 mkdir -p output/c3d/v_ApplyEyeMakeup_g01_c01
 mkdir -p output/c3d/v_BaseballPitch_g01_c01
-GLOG_logtosterr=1 ../../../../caffe/build/tools/extract_features prototxt/c3d_sport1m_feature_extractor_video.prototxt conv3d_deepnetA_sport1m_iter_1900000 0 50 1 prototxt/output_list_video_prefix.txt fc7-1 fc6-1 prob
+if [ ! -f c3d_resnet18_sports1m_r2_iter_2800000.caffemodel ];then
+	wget https://www.dropbox.com/s/qqfrg6h44d4jb46/c3d_resnet18_sports1m_r2_iter_2800000.caffemodel
+fi
+
+GLOG_logtosterr=1 ../../../../caffe/build/tools/extract_features prototxt/c3d_sport1m_feature_extractor_video.prototxt c3d_resnet18_sports1m_r2_iter_2800000.caffemodel 0 50 1 prototxt/output_list_video_prefix.txt fc7-1 fc6-1 prob
